bbc8e2e4eeaeac58a3a54f5de5779b6c72b31b0a
jetsonhacks <jim@jetsonhacks.com>
2016-08-15 23:22:04 -0700

    Arm 32 TX1

---
+++ compileGolangArm32.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Compile Go (Golang) from source on the Jetson Dev Kit
+# To build a version > go 1.5, a go 1.4 version is used to 
+# bootstrap the compiler. The bootstrap is cross compiled from
+# another machine, as aarch64 is not supported by 1.4
+# This is for Jetson TX1 - 32 bit
+cd $HOME
+tar xvjf compileGolang/go-linux-arm-bootstrap.tbz
+export GOROOT_BOOTSTRAP=$HOME/go-linux-arm-bootstrap
+cd $HOME
+# After the bootstrap is copied, then the desired version can be built
+git clone https://github.com/golang/go.git
+cd go
+# Latest stable is go 1.6
+git checkout release-branch.go1.6
+cd src
+export GOARCH=arm
+export GOARM=7
+# Make go for 32 bit
+export GOHOSTARCH=arm
+./all.bash
+# Setup environment variables
+echo "export GOROOT=$HOME/go" >> ~/.bashrc
+echo "export PATH=$PATH:$HOME/go/bin" >> ~/.bashrc
+# Issues with source bashrc
+source ~/.bashrc
+GOROOT=$HOME/go
+PATH=$PATH:$GOROOT/bin
+
+
+
